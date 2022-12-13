06adfff4866cfb5a93ecbd0b57777c4a1c1cb989
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-13 08:38:18 -0400

    hook, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import Thanks from "./components/Thanks";
+import ReviewForm from "./components/ReviewForm";
+import UserForm from "./components/UserForm";
 import "./App.css";
 
+//Hooks
+import { useForm } from "./hooks/useForm";
+
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const { currentStep, currentComponents } = useForm(formComponents);
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliaçção</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form>
+          <div className="inputs-container">{currentComponents}</div>
+          <div className="action">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/components/Thanks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Thanks = () => {
+  return <h2>Thanks</h2>;
+};
+
+export default Thanks;
